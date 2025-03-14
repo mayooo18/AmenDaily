c42d7b10b384b7fe429998db53ba904dee211ba2
Mario Mendez <mendezmario2018@gmail.com>
2025-03-14 13:33:17 -0500

    added index and unique pins so seperate people with same name

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
-app.secretKey = "key"
+app.secret_key = "key"
 
 @app.route('/', methods=['GET', 'POST'])
 
 def login():
     if request.method == 'POST':
         username = request.form["username"].strip()
-        if username:
-            session["user"] = username
+        pin = request.form["pin"].strip() #added pin to login for privacy
+
+        if username and pin:
+            session["user"] = f"{username}_{pin}"#makes the username and pin a unique id
+            
             return redirect(url_for('home'))
     return render_template('login.html')
 
 @app.route('/home')
 def home():
-    if "user" in session:
+    if "user" not in session:
         return redirect(url_for('login')) #if no user returns back to login
+    
     username = session["user"]
     return render_template('index.html', username=username)
 
