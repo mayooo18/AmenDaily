66038ae3efa2213fb9ea8874723f9065f59b3dc6
Mario Mendez <mendezmario2018@gmail.com>
2025-03-15 01:49:05 -0500

    did somw css and html styling for login

--- app.py
+++ app.py
@@ -8,11 +8,9 @@ app.secret_key = "key"
 def login():
     if request.method == 'POST':
         username = request.form["username"].strip()
-        pin = request.form["pin"].strip() #added pin to login for privacy
-
+        pin = request.form["pin"].strip()
         if username and pin:
-            session["user"] = f"{username}_{pin}"#makes the username and pin a unique id
-            
+            session["user"] = username
             return redirect(url_for('home'))
     return render_template('login.html')
 
@@ -20,14 +18,14 @@ def login():
 def home():
     if "user" not in session:
         return redirect(url_for('login')) #if no user returns back to login
-    
     username = session["user"]
     return render_template('index.html', username=username)
 
 @app.route('/logout')
 def logout():
-    session.pop("user",None)
-    return redirect(url_for('login'))
+    session.pop("user", None)  # Logs user out
+    return redirect(url_for('login'))  # Redirect back to login
+
 
 if __name__ == '__main__':
     app.run(debug=True)
